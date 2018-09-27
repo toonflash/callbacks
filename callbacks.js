57ac6348d765ad2ddd2b5445086daebeff419868
toonflash <toonflash@hotmail.com>
2018-09-27 19:16:43 +0000

    Print the index of Waldo when found

--- callbacks.js
+++ callbacks.js
@@ -1,1 +1,25 @@
-// callbacks
+
+function findWaldo(arr, found) {
+
+// FOR LOOP VERSION //
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       var index = i;
+//       found(index);   // execute callback
+//     }
+//   }
+
+// FOREACH VERSION //
+arr.forEach(function(name, index){
+    if (name === "Waldo") {
+        found(index);
+    }
+  });
+}
+
+function actionWhenFound(index) {
+    var currentIndex = index;
+  console.log("Found Waldo at index " + currentIndex + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
